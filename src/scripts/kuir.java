70316acc1499e3b019e7ac2162534985cfe99b4c
DESKTOP-0DDVE4M\POTATOY <potatoy@outlook.kr>
2022-03-31 09:17:41 +0900

    searcher.java 생성

--- src/scripts/kuir.java
+++ src/scripts/kuir.java
@@ -35,5 +35,14 @@ public class kuir{
 		else if(command.equals("-i")) {//index.xml 읽기+index.post 만들기  -i ./index.xml
 			indexer makepost = new indexer(path);
 		}
+		else if(command.equals("-s")) {
+			if(args.length>2) {
+				String pluscommand = args[2];
+				String question = args[3];
+				if(pluscommand.equals("-q")) {
+					searcher searcher = new searcher(path,question);
+				}
+			}
+		}
 	}
 }
---
+++ src/scripts/searcher.java
@@ -0,0 +1,7 @@
+package scripts;
+
+public class searcher {
+	public searcher(String inputpath, String mainquery) {
+		
+	}
+}
