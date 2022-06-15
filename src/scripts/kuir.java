769184cd156193b8d13cd898fe74af0926b471af
DESKTOP-0DDVE4M\POTATOY <potatoy@outlook.kr>
2022-06-15 16:33:39 +0900

    1. 15주차 실습 완료-이클립스에서 OpenNaverMovie.java 를 Run 하면 검색가능 / 예시 결과는 15주차 검색결과.png에 있음 2. 외부 jar를 프로젝트 폴더 내에 위치시키고 상대경로로 빌드하게 만들어 프로그램 실행에 지장이 없도록 함

--- src/scripts/kuir.java
+++ src/scripts/kuir.java
@@ -1,27 +1,5 @@
 package scripts;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.snu.ids.kkma.index.Keyword;
-import org.snu.ids.kkma.index.KeywordExtractor;
-import org.snu.ids.kkma.index.KeywordList;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 public class kuir{
 	public static void main(String[] args) {
 		String command = args[0];
